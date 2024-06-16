987c35abb0dae56e17b1bc51115997ae16246f08
Kavikaveesha <140581819+Kavikaveesha@users.noreply.github.com>
2024-06-17 02:05:28 +0530

    Mvc Architecture Done
    I have use MVC architecture for our project.Refer this architecture .You should create your branch(your name) and open project and start to develop.good luck!

---
+++ N-Library/src/java/com/team2/controller/utill/assetsUrl.java
@@ -0,0 +1,21 @@
+package com.team2.controller.utill;
+
+import javax.servlet.http.HttpServletRequest;
+/**
+ *
+ * @author Ashani
+ */
+public class assetsUrl {
+    
+             public static String giveUrl(HttpServletRequest request, String path)
+             {
+                 
+                    
+                    String fileUrl = request.getScheme() + "://" + request.getServerName()+ ":" + request.getServerPort() + request.getServletContext() .getContextPath() + "/";
+//                    String fileUrl = request.getScheme() + "://" + request.getServerName() + "/" + request.getServletContext().getContextPath() + "/";
+                    String url = fileUrl + path;
+                    
+                 return url;
+             }
+             
+}
