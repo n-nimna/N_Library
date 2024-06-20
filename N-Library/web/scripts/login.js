dc2ddd21e8c35d47c0aac442ad9b704c432dcf4c
Nimeshi Dilshara <120201164+n-nimna@users.noreply.github.com>
2024-06-20 23:48:26 +0530

    create login page

---
+++ N-Library/web/scripts/login.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Add your login logic here
+    console.log('Email:', email);
+    console.log('Password:', password);
+});
